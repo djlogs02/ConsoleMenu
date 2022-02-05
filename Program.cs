388c4e68af941fdb6ea87f64947bf96c5423ca16
Derrick Logsdon <derrick@derricklogsdon.com>
2022-02-05 00:41:51 -0500

    Updated Menu Names

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace ConsoleMenu
     {
         static void Main(string[] args)
         {
-            Menu subMenu1 = new Menu("SubMenu1");
-            subMenu1.AddAction("Whats My Name", () => { Console.Out.WriteLine("Aaron is My name"); });
+            Menu subMenu1 = new Menu("Aaron's ");
+            subMenu1.AddAction("Whats My Name", () => { Console.Clear(); Console.Out.WriteLine("Aaron is My name"); });
 
-            Menu subMenu2 = new Menu("SubMenu2");
-            subMenu2.AddAction("Option A", () => { Console.Out.WriteLine("Derrick is My name"); });
+            Menu subMenu2 = new Menu("Derrick's ");
+            subMenu2.AddAction("Whats My Name", () => { Console.Clear(); Console.Out.WriteLine("Derrick is My name"); });
 
             Menu mainMenu = new Menu("Main");
             mainMenu.AddAction("Aaron", () => { subMenu1.Prompt(mainMenu); });
