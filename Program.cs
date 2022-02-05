01840d605a0a22ccf22256eff919ce2654240918
Derrick Logsdon <derrick@derricklogsdon.com>
2022-02-05 00:38:34 -0500

    Changed Names

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace ConsoleMenu
         static void Main(string[] args)
         {
             Menu subMenu1 = new Menu("SubMenu1");
-            subMenu1.AddAction("Option1", () => { Console.Out.WriteLine("Option1 Selected"); });
+            subMenu1.AddAction("Whats My Name", () => { Console.Out.WriteLine("Aaron is My name"); });
 
             Menu subMenu2 = new Menu("SubMenu2");
-            subMenu2.AddAction("Option A", () => { Console.Out.WriteLine("Option A Selected"); });
+            subMenu2.AddAction("Option A", () => { Console.Out.WriteLine("Derrick is My name"); });
 
             Menu mainMenu = new Menu("Main");
-            mainMenu.AddAction("SubMenu1", () => { subMenu1.Prompt(mainMenu); });
-            mainMenu.AddAction("SubMenu2", () => { subMenu2.Prompt(mainMenu); });
+            mainMenu.AddAction("Aaron", () => { subMenu1.Prompt(mainMenu); });
+            mainMenu.AddAction("Derrick", () => { subMenu2.Prompt(mainMenu); });
 
             mainMenu.Prompt();
         }
